53cd6aa2be1c6903d45b537d662ed6f5233a5699
hoon32 <k@jdlab.org>
2015-07-28 17:08:25 +0900

    fix code

--- conf.py
+++ conf.py
@@ -113,8 +113,8 @@ html_theme_options = {
 
 # Add any paths that contain custom themes here, relative to this directory.
 
-#html_theme_path = ["sphinx_rtd_theme"]
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = ["sphinx_rtd_theme"]
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_context = {
 	'css_files':[
